201bf417de7f8e9cf022f4678020457854698b73
Heitor Hatakeyama <heitor_suh@hotmail.com>
2025-03-05 13:48:16 -0300

    Ajustes migrations

---
+++ database/migrations/2025_03_05_134420_create_password_reset_tokens_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   */
+  public function up() {
+    Schema::create('password_reset_tokens', function (Blueprint $table) {
+      $table->string('email')->primary();
+      $table->string('token');
+      $table->timestamp('created_at')->nullable();
+    });
+    if (!Schema::hasTable('sessions')) {
+      Schema::create('sessions', function (Blueprint $table) {
+        $table->string('id')->primary();
+        $table->foreignId('user_id')->nullable()->index();
+        $table->string('ip_address', 45)->nullable();
+        $table->text('user_agent')->nullable();
+        $table->longText('payload');
+        $table->integer('last_activity')->index();
+      });
+    }
+  }
+
+  public function down() {
+    Schema::dropIfExists('password_reset_tokens');
+    Schema::dropIfExists('sessions');
+  }
+};
